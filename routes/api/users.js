6c89f7cbdb61adf303720c757ecc2cd45c5f344f
Joseph Gotengco <45640953+JosephGotengco@users.noreply.github.com>
2019-12-02 23:55:47 -0800

    folder

--- routes/api/users.js
+++ routes/api/users.js
@@ -5,7 +5,7 @@ const multer = require('multer')
 
 var storage = multer.diskStorage({
     destination: function (req, file, cb) {
-        cb(null, '/tmp/my-uploads')
+        cb(null, './uploads/')
     },
     filename: function (req, file, cb) {
         cb(null, file.fieldname + '-' + Date.now())
